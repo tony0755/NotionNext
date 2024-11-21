c0a29a7b4fc5f472514300951136863f9cddaec8
tony0755 <szu2357@gmail.com>
2024-11-21 19:43:33 +0800

    Update config.js

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -2,19 +2,19 @@ const CONFIG = {
   // 封面大图
   COMMERCE_HOME_BANNER_ENABLE: true,
 
-  COMMERCE_TEXT_CENTER_TITLE: 'Product Center', // 中间产品区块标题
+  COMMERCE_TEXT_CENTER_TITLE: '深圳——人工智能先锋城市', // 中间产品区块标题
   COMMERCE_TEXT_CENTER_DESCRIPTION:
-    'The vision of NotionNext is to help you effortlessly and seamlessly build your own website, amplifying the value of your brand.', // 中间产品区块文字描述
-  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: 'Product Categories', // 左侧产品分类标题
-  COMMERCE_TEXT_FOOTER_TITLE: 'Contact US', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
-  COMMERCE_TEXT_FOOTER_MENU_1: 'Product Center', // COMMERCE主题页脚左侧菜单标题1
-  COMMERCE_TEXT_FOOTER_MENU_2: 'About US', // COMMERCE主题页脚左侧菜单标题2
+    '深圳作为中国改革开放的前沿，始终引领科技创新潮流。人工智能产业自2010年代初在深圳蓬勃兴起，依托强大的电子信息产业基础和世界一流的创新环境，迅速成为全国领先的AI技术高地。从无人驾驶、智能机器人到语音识别，深圳的AI成果屡获瞩目。近年来，龙头企业与科研机构协同发力，深圳在芯片设计、算法开发、智能制造等领域加速突破，吸引了全球顶尖人才与资本。展望未来，深圳将以国家级人工智能创新试验区为契机，构建更加完善的创新生态，推动AI深度赋能各行各业。作为未来世界级人工智能中心，深圳注定在全球科技竞赛中书写新的辉煌篇章！', // 中间产品区块文字描述
+  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: '', // 左侧产品分类标题
+  COMMERCE_TEXT_FOOTER_TITLE: '', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
+  COMMERCE_TEXT_FOOTER_MENU_1: '', // COMMERCE主题页脚左侧菜单标题1
+  COMMERCE_TEXT_FOOTER_MENU_2: '', // COMMERCE主题页脚左侧菜单标题2
 
   COMMERCE_FOOTER_RIGHT_IMG_URL: null, // 显示页脚右侧的图片，通常放二维码
   COMMERCE_FOOTER_RIGHT_TEXT: null, // 页脚右侧图片下的文字描述
 
-  COMMERCE_HOME_POSTS_COUNT: 9, // 首页展示商品数
-  COMMERCE_CONTACT_WHATSAPP_SHOW: true, // 是否展示whatsapp联系按钮 请配置 CONTACT_WHATSAPP
-  COMMERCE_CONTACT_TELEGRAM_SHOW: true // 联系栏展示telegram按钮 请配置 CONTACT_TELEGRAM
+  COMMERCE_HOME_POSTS_COUNT: 16, // 首页展示商品数
+  COMMERCE_CONTACT_WHATSAPP_SHOW: false, // 是否展示whatsapp联系按钮 请配置 CONTACT_WHATSAPP
+  COMMERCE_CONTACT_TELEGRAM_SHOW: false // 联系栏展示telegram按钮 请配置 CONTACT_TELEGRAM
 }
 export default CONFIG

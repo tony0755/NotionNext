30b5adee1288f2273e7463365798f0cde648db76
tony0755 <szu2357@gmail.com>
2024-11-23 10:41:37 +0800

    Update config.js

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -4,18 +4,17 @@ const CONFIG = {
 
   COMMERCE_TEXT_CENTER_TITLE: '深圳——人工智能先锋城市', // 中间产品区块标题
   COMMERCE_TEXT_CENTER_DESCRIPTION:
-    '深圳作为中国改革开放的前沿，始终引领科技创新潮流。近年来，龙头企业与科研机构协同发力，深圳在芯片设计、算法开发、智能制造等领域加速突破，吸引了全球顶尖人才与资本。
-展望未来，深圳将以国家级人工智能创新试验区为契机，构建更加完善的创新生态，推动AI深度赋能各行各业。作为未来世界级人工智能中心，深圳注定在全球科技竞赛中书写新的辉煌篇章！', // 中间产品区块文字描述
+    '深圳作为中国改革开放的前沿，始终引领科技创新潮流。近年来，龙头企业与科研机构协同发力，深圳在芯片设计、算法开发、智能制造等领域加速突破，吸引了全球顶尖人才与资本。展望未来，深圳将以国家级人工智能创新试验区为契机，构建更加完善的创新生态，推动AI深度赋能各行各业。作为未来世界级人工智能中心，深圳注定在全球科技竞赛中书写新的辉煌篇章！', // 中间产品区块文字描述
   COMMERCE_TEXT_CENTER_CATEGORY_TITLE: '人工智能', // 左侧产品分类标题
-  COMMERCE_TEXT_FOOTER_TITLE: '', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
-  COMMERCE_TEXT_FOOTER_MENU_1: '', // COMMERCE主题页脚左侧菜单标题1
-  COMMERCE_TEXT_FOOTER_MENU_2: '', // COMMERCE主题页脚左侧菜单标题2
+  COMMERCE_TEXT_FOOTER_TITLE: null, // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
+  COMMERCE_TEXT_FOOTER_MENU_1: null, // COMMERCE主题页脚左侧菜单标题1
+  COMMERCE_TEXT_FOOTER_MENU_2: null, // COMMERCE主题页脚左侧菜单标题2
 
   COMMERCE_FOOTER_RIGHT_IMG_URL: null, // 显示页脚右侧的图片，通常放二维码
   COMMERCE_FOOTER_RIGHT_TEXT: null, // 页脚右侧图片下的文字描述
 
   COMMERCE_HOME_POSTS_COUNT: 16, // 首页展示商品数
   COMMERCE_CONTACT_WHATSAPP_SHOW: false, // 是否展示whatsapp联系按钮 请配置 CONTACT_WHATSAPP
-  COMMERCE_CONTACT_TELEGRAM_SHOW: false // 联系栏展示telegram按钮 请配置 CONTACT_TELEGRAM
+  COMMERCE_CONTACT_TELEGRAM_SHOW: false,  // 联系栏展示telegram按钮 请配置 CONTACT_TELEGRAM
 }
 export default CONFIG
